a151ed4c78b4b4b6ec1ca605cc62e4fd8ada5df9
ffittschen <ffittschen@Florians-MacBook-Pro.local>
2014-06-11 00:02:48 +0200

    contact form

--- de/reservations/contact.php
+++ de/reservations/contact.php
@@ -1,41 +1,87 @@
 <?php
 
-    $data = array();      // array to pass back data
+$errors         = array();      // array to hold validation errors
+$data           = array();      // array to pass back data
 
-    $data['salutation'] = $_POST['salutation']; // (r)
-    $data['lastname'] = $_POST['lastname']; // (r)
-    $data['firstname'] = $_POST['firstname']; // (r)
-    $data['birthday'] = $_POST['birthday']; // (r)
-    $data['phone'] = $_POST['phone'];        
-    $data['email'] = $_POST['email']; // (r)
-    $data['date'] = $_POST['date']; // (r)
-    $data['adults'] = $_POST['adults']; // (r)
-    $data['children'] = $_POST['children']; // (r)
-    $data['message'] = $_POST['message'];       
+// validate the variables ======================================================
+    if (empty($_POST['salutation']))
+        $errors['salutation'] = 'Salutation is required.';
+    else
+        $data['salutation'] = $_POST['salutation']; // (r)
+
+    if (empty($_POST['lastname']))
+        $errors['lastname'] = 'Lastname is required.';
+    else
+        $data['lastname'] = $_POST['lastname']; // (r)
+
+    if (empty($_POST['firstname']))
+        $errors['firstname'] = 'Firstname is required.';
+    else
+        $data['firstname'] = $_POST['firstname']; // (r)
+
+    if (empty($_POST['birthday']))
+        $errors['birthday'] = 'Birthday is required.';
+    else
+        $data['birthday'] = $_POST['birthday']; // (r)
+
+    if (!empty($_POST['lastname']))
+        $data['phone'] = $_POST['phone'];        
+
+    if (empty($_POST['email']))
+        $errors['email'] = 'Email is required.';
+    else
+        $data['email'] = $_POST['email']; // (r)
+
+    if (empty($_POST['date']))
+        $errors['date'] = 'Date is required.';
+    else
+        $data['date'] = $_POST['date']; // (r)
+
+    if (empty($_POST['adults']))
+        $errors['adults'] = 'Adults is required.';
+    else
+        $data['adults'] = $_POST['adults']; // (r)
+
+    if (empty($_POST['children']))
+        $errors['children'] = 'Children is required.';
+    else
+        $data['children'] = $_POST['children']; // (r)
+
+    if (!empty($_POST['lastname']))
+        $data['message'] = $_POST['message'];
 
 // return a response ===========================================================
 
-    $data['success'] = true;
-    $data['message'] = 'Success!';
+    // response if there are errors
+        if ( ! empty($errors)) {
 
-    /* Empfänger */
-    $empfaenger = 'ffittschen@gmail.com';
+            // if there are items in our errors array, return those errors
+            $data['success'] = false;
+            $data['errors']  = $errors;
+        } else {
 
-    /* Absender */
-    $absender = 'info@ferienwohnung-wimberger.de';
+            // if there are no errors, return a message
+            $data['success'] = true;
+            $data['message'] = 'Success!';
 
-    /* Betreff */
-    $subject = 'Neue Anfrage von' + $data['firstname'] + ' ' + $data['lastname'] + '';
+            /* Empfänger */
+            $empfaenger = 'ffittschen@gmail.com';
 
-    /* Nachricht */
-    $message = $data;
+            /* Absender */
+            $absender = 'info@ferienwohnung-wimberger.de';
 
-    /* Verschicken der Mail */
-    mail($empfaenger, $subject, $message, "From: anfragen@ferienwohnung-wimberger.de", '-f info@ferienwohnung-wimberger.de');
+            /* Betreff */
+            $subject = 'Neue Anfrage von' + $data['firstname'] + ' ' + $data['lastname'] + '';
+
+            /* Nachricht */
+            $message = $data;
+
+            /* Verschicken der Mail */
+            mail($empfaenger, $subject, $message, 'From: anfragen@ferienwohnung-wimberger.de', '-f info@ferienwohnung-wimberger.de');
+        }
 
     // return all our data to an AJAX call
     echo json_encode($data);
-
     
 ?>
 

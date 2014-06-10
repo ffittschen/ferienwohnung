9dd143ff183e1eaf491ea9e8bdb25c62ce1bf15d
ffittschen <ffittschen@Florians-MacBook-Pro.local>
2014-06-10 20:12:21 +0200

    contact php update

--- de/reservations/contact.php
+++ de/reservations/contact.php
@@ -18,10 +18,23 @@
     $data['success'] = true;
     $data['message'] = 'Success!';
 
-    $to = "ffittschen@gmail.com";
-    $subject = "Neue Anfrage von $firstname $lastname";
+    $firstname = $data['firstname'];
+    $lastname = $data['lastname'];
 
-    mail($to, $subject, $data, "From: anfragen@ferienwohnung-wimberger.de\r\n");
+    /* Empfänger */
+    $empfaenger = 'ffittschen@gmail.com';
+
+    /* Absender */
+    $absender = 'info@ferienwohnung-wimberger.de';
+
+    /* Betreff */
+    $subject = 'Neue Anfrage von' + $firstname + $lastname + '';
+
+    /* Nachricht */
+    $message = $data;
+
+    /* Verschicken der Mail */
+    mail($empfaenger, $subject, $message, 'From: anfragen@ferienwohnung-wimberger.de', '-f info@ferienwohnung-wimberger.de');
 
     // return all our data to an AJAX call
     echo json_encode($data);
